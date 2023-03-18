1ba0cc4238aaaaaac98c072eef7c071719cc43e6
Simon Brown <simon@structurizr.com>
2023-03-18 16:53:12 +0000

    Change opacity of double-click markers.

--- src/js/structurizr-diagram.js
+++ src/js/structurizr-diagram.js
@@ -1114,7 +1114,7 @@ structurizr.ui.Diagram = function(id, diagramIsEditable, constructionCompleteCal
             const domElement = $('#' + linkView.id);
 
             const svg =
-                '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" opacity="0.75" class="structurizrNavigation" viewBox="0 0 16 16">' +
+                '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="structurizrNavigation" viewBox="0 0 16 16">' +
                 '<path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>' +
                 '<path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>' +
                 '</svg>';
@@ -1559,7 +1559,6 @@ structurizr.ui.Diagram = function(id, diagramIsEditable, constructionCompleteCal
         cell.attributes.attrs['.structurizrDescription']['lineHeight'] = lineHeight;
 
         cell.attributes.attrs['.structurizrNavigation']['color'] = navigationColor;
-        cell.attributes.attrs['.structurizrNavigation']['opacity'] = 0.75;
         cell.attributes.attrs['.structurizrNavigation']['ref-y'] = navigationRefY;
 
         cell._computedStyle = {};
